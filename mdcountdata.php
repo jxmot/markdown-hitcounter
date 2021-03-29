efd3ea11b26741052ed5df45f1a1cc3b9bf4f2ad
jxmot <webdev@stupiddog.net>
2021-03-28 23:16:37 -0500

    added selectable sorting, ?sort=[a|d]

--- mdcountdata.php
+++ mdcountdata.php
@@ -51,14 +51,17 @@ define('LOG_FOLDER', './logs/');
 $queries = array();
 
 parse_str($_SERVER['QUERY_STRING'], $queries);
+$id   = (isset($queries['id'])   ? strtolower($queries['id'])   : null);
+$sort = (isset($queries['sort']) ? strtolower($queries['sort']) : null);
 
-$_id = (isset($queries['id']) ? $queries['id'] : null);
+//$id   = null;
+//$sort = 'd';
+
+$_idlist = json_decode(file_get_contents(VALID_COUNTERS));
+$idlist  = array_map('strtolower', $_idlist->valid);
 
 // did we get a counter ID?
-if($_id !== null) {
-    $id = strtolower($_id);
-    $_idlist = json_decode(file_get_contents(VALID_COUNTERS));
-    $idlist = array_map('strtolower', $_idlist->valid);
+if($id !== null) {
     // is the counter ID known?
     if(in_array($id, $idlist)) {
         // build the log file name from the ID and "_count.log"
@@ -83,31 +86,50 @@ if($_id !== null) {
         $result = '[{"error":true,"msg":"['.$id.'] not found in '.VALID_COUNTERS.'"}]';
     }
 } else {
-    $_idlist = json_decode(file_get_contents('./counters.json'));
-    $idlist = array_map('strtolower', $_idlist->valid);
-
     $out = '';
     $ix = 0;
     foreach($idlist as $id) {
         // build the log file name from the ID and "_count.log"
-        $counter = $id . '_count.json';
+        //$counter = $id . '_count.json';
         // path + counter file
-        $cntfile = LOG_FOLDER . $counter;
+        $cntfile = LOG_FOLDER . $id . '_count.json';;
         if(file_exists($cntfile)) {
             $filecnt = fopen($cntfile,'r');
-            // get 128 characters, it's unlikely that counter
+            // get 128 characters, it's unlikely that a counter
             // would get that big.
             $json = fgets($filecnt,128);
             fclose($filecnt);
-
+            // add a counter to the array
             $out = $out . ($ix > 0 ? ',' : '[');
             $out = $out . '{"id":"'.$id.'","data":';
             $out = $out . $json . '}';
-
+            // only used above to decide if we need a comma
             $ix += 1;
         }
     }
     $out = $out . ']';
+
+    // is sorting selected?
+    if($sort !== null) {
+        $data = json_decode($out);
+
+        // ascendind
+        if($sort === 'a' || $sort === '') {
+            usort($data, function($a, $b) {
+                if ($a->data->count === $b->data->count) return 0;
+                return (($a->data->count < $b->data->count)?-1:1);
+            });
+        }
+        // descending
+        if($sort === 'd') {
+            usort($data, function($a, $b) {
+                if ($a->data->count === $b->data->count) return 0;
+                return (($a->data->count > $b->data->count)?-1:1);
+            });
+        }
+        // done!
+        $out = json_encode($data);
+    }
     $result = $out;
 }
 header("HTTP/1.0 200 OK");
