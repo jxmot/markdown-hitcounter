2f45b0b9697722beae7103909879c72788e0c18b
jxmot <webdev@stupiddog.net>
2021-03-30 11:56:13 -0500

    added ability to limit qty returned in sortings, added comments

--- mdcountdata.php
+++ mdcountdata.php
@@ -1,5 +1,9 @@
 <?php
 /*
+
+    See https://github.com/jxmot/markdown-hitcounter for detailed
+    information not contained here.
+
     Usage:
 
         Return data for all counters -
@@ -11,6 +15,30 @@
         Return data for a specific counter -
 
         GET http[s]://your-server/path-to-file/mdcountdata.php?id=testtest
+
+            OR
+    
+        Return data for all counters, but sorted -
+
+        GET http[s]://your-server/path-to-file/mdcountdata.php?[csort|tsort|isort]=[a|d]
+
+            Where:
+                csort - sort by count 
+                tsort - sort by time of last count 
+                isort - sort by counter ID 
+
+                a - sort ascending 
+                d - sort descending 
+
+
+        Return sorted data, but limit the quantity of counters returned
+
+        GET http[s]://your-server/path-to-file/mdcountdata.php?csort=a|[&limit=[1-n]]
+
+            Where:
+                limit - limit the quantity of counters to return
+                1-n   - quantity
+
     
     Report data returned:
         
@@ -46,7 +74,7 @@ function tzone() {
     return $tmp->tz;
 }
 
-function countsort($sort, $out) {
+function countsort($sort, $out, $limit = null) {
     $data = json_decode($out);
     // ascending
     if($sort === 'a' || $sort === '') {
@@ -63,10 +91,13 @@ function countsort($sort, $out) {
         });
     }
     // done!
-   return json_encode($data);
+    if($limit !== null && ($limit > 0 && $limit < count($data))){
+        $data = array_slice($data, 0, $limit);
+    }
+    return json_encode($data);
 }
 
-function timesort($sort, $out) {
+function timesort($sort, $out, $limit = null) {
     $data = json_decode($out);
     // ascending
     if($sort === 'a' || $sort === '') {
@@ -83,10 +114,13 @@ function timesort($sort, $out) {
         });
     }
     // done!
-   return json_encode($data);
+    if($limit !== null && ($limit > 0 && $limit < count($data))){
+        $data = array_slice($data, 0, $limit);
+    }
+    return json_encode($data);
 }
 
-function idsort($sort, $out) {
+function idsort($sort, $out, $limit = null) {
     $data = json_decode($out);
     // ascending
     if($sort === 'a' || $sort === '') {
@@ -103,20 +137,31 @@ function idsort($sort, $out) {
         });
     }
     // done!
-   return json_encode($data);
+    if($limit !== null && ($limit > 0 && $limit < count($data))){
+        $data = array_slice($data, 0, $limit);
+    }
+    return json_encode($data);
 }
 
 // MUST be done like this for PHP files that are 'linked'
 $queries = array();
-
 parse_str($_SERVER['QUERY_STRING'], $queries);
+// return a single counter by ID
 $id    = (isset($queries['id'])    ? strtolower($queries['id'])    : null);
+// return all counters, ordered by count
 $csort = (isset($queries['csort']) ? strtolower($queries['csort']) : null);
+// return all counters, ordered by time of last count
 $tsort = (isset($queries['tsort']) ? strtolower($queries['tsort']) : null);
+// return all counters, ordered by ID
 $isort = (isset($queries['isort']) ? strtolower($queries['isort']) : null);
+// for sorts, limit number of counters returned
+$limit = (isset($queries['limit']) ? $queries['limit'] : null);
 
 //$id    = null;
 //$csort = 'd';
+//$tsort = null;
+//$isort = null;
+//$limit = 1;
 
 $_idlist = json_decode(file_get_contents(VALID_COUNTERS));
 $idlist  = array_map('strtolower', $_idlist->valid);
@@ -171,17 +216,17 @@ if($id !== null) {
     // is sorting selected?
     if($csort !== null) {
         // by count
-        $out = countsort($csort, $out);
+        $out = countsort($csort, $out, $limit);
     }
 
     if($tsort !== null) {
         // by time
-        $out = timesort($tsort, $out);
+        $out = timesort($tsort, $out, $limit);
     }
 
     if($isort !== null) {
         // by ID
-        $out = idsort($isort, $out);
+        $out = idsort($isort, $out, $limit);
     }
 
     $result = $out;
